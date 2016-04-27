3b83f7f8c130979c7df3f1eda093f214917fc97e
itegulov <ditegulov@gmail.com>
2016-04-27 21:42:01 +0300

    Revert "Pack frontend client in Electron to make in 'native'"
    This reverts commit b8a6597300c1cd05b987beffc7386179f065b845.

--- build.sbt
+++ build.sbt
@@ -1,5 +1,4 @@
 import sbt.Project.projectToRef
-import java.nio.charset.Charset
 
 name := "RecursiveParserWeb"
 
@@ -25,61 +24,15 @@ lazy val server = (project in file("server")).settings(
   aggregate(clients.map(projectToRef): _*).
   dependsOn(sharedJvm)
 
-val electronMainPath = SettingKey[File]("electron-main-path", "The absolute path where to write the Electron application's main.")
-
-val electronMain = TaskKey[File]("electron-main", "Generate Electron application's main file.")
-
 lazy val client = (project in file("client")).settings(
   scalaVersion := scalaV,
-  persistLauncher in Compile := true,
+  persistLauncher := true,
   persistLauncher in Test := false,
-
-  electronMainPath := {
-    baseDirectory.value / "electron-app" / "main.js"
-  },
-
-  electronMain := {
-    // TODO here we rely on the files written on disk but it would be better to be able to get the actual content directly
-    // from the tasks. I am not sure it's possible just yet though.
-    val jsCode: String = IO.read((fastOptJS in Compile).value.data, Charset.forName("UTF-8"))
-    val launchCode = IO.read((packageScalaJSLauncher in Compile).value.data, Charset.forName("UTF-8"))
-    // we don't need jsDeps here but want it to be generated anyway so that we can start the Electron app right away
-    val jsDeps = (packageJSDependencies in Compile).value
-
-    // hack to get require and __dirname to work in the main process
-    // see https://gitter.im/scala-js/scala-js/archives/2015/04/25
-    val hack =
-      """
-      var addGlobalProps = function(obj) {
-        obj.require = require;
-        obj.__dirname = __dirname;
-      }
-      if((typeof __ScalaJSEnv === "object") && typeof __ScalaJSEnv.global === "object") {
-        addGlobalProps(__ScalaJSEnv.global);
-      } else if(typeof  global === "object") {
-        addGlobalProps(global);
-      } else if(typeof __ScalaJSEnv === "object") {
-        __ScalaJSEnv.global = {};
-        addGlobalProps(__ScalaJSEnv.global);
-      } else {
-        var __ScalaJSEnv = { global: {} };
-        addGlobalProps(__ScalaJSEnv.global)
-      }
-      """
-
-    val dest = electronMainPath.value
-    IO.write(dest, hack + jsCode + launchCode, Charset.forName("UTF-8"))
-    dest
-  },
-
   libraryDependencies ++= Seq(
     "org.scala-js" %%% "scalajs-dom" % "0.8.0",
     "be.doeraene" %%% "scalajs-jquery" % "0.9.0",
     "org.singlespaced" %%% "scalajs-d3" % "0.3.1"
-  ),
-
-  jsDependencies += RuntimeDOM,
-  skip in packageJSDependencies := false
+  )
 ).enablePlugins(ScalaJSPlugin).
   dependsOn(sharedJs)
 
--- client/src/main/scala/com/itegulov/parser/client/MainJS.scala
+++ client/src/main/scala/com/itegulov/parser/client/MainJS.scala
@@ -7,10 +7,6 @@ import org.scalajs.dom.{KeyboardEvent, html}
 import org.scalajs.jquery._
 import org.singlespaced.d3js.d3
 
-import scala.scalajs.js
-import js.Dynamic.{global => g}
-import io.atom.electron._
-
 import scala.collection.mutable.ArrayBuffer
 import scala.scalajs.js
 import scala.scalajs.js.JSApp
@@ -23,109 +19,72 @@ object MainJS extends JSApp {
 
   @JSExport
   def main: Unit = {
-    val app = g.require("app").asInstanceOf[App] // Module to control application life.
-
-    // Report crashes to our server.
-    g.require("crash-reporter").start(js.Dynamic.literal(companyName = "Daniyar Itegulov", submitURL = "none"))
-
-    // Keep a global reference of the window object, if you don't, the window will
-    // be closed automatically when the JavaScript object is GCed.
-    var mainWindow: BrowserWindow = null
-
-    // Quit when all windows are closed.
-    app.on("window-all-closed", { () =>
-      // On OS X it is common for applications and their menu bar
-      // to stay active until the user quits explicitly with Cmd + Q
-      if (Process.platform != "darwin") {
-        app.quit()
+    val sentenceInput = dom.document.getElementById("sentence_input").asInstanceOf[html.Input]
+    sentenceInput.onkeypress = (e: KeyboardEvent) => {
+      if (e.charCode == 13) {
+        renderText(sentenceInput.value)
+        e.preventDefault()
       }
-    })
-
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    app.on("ready", () => {
-      // Create the browser window.
-      mainWindow = BrowserWindow(width = 1024, height = 768)
-
-      // and load the index.html of the app.
-      mainWindow.loadURL("file://" + g.__dirname + "/index.html")
-
-      // Open the devtools.
-      mainWindow.openDevTools()
-
-      // Emitted when the window is closed.
-      val _ = mainWindow.on("closed", () =>
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-      )
-
-    })
-
-//    val ipc = g.require("electron").ipcMain
-//    ipc.on("parseAction", (event: js.Any, data: String) => {
-//      renderText(data)
-//    })
+    }
+    renderText(SharedMessages.defaultExpression)
   }
 
-//  @ScalaJSDefined
-//  class Tree(val node: String, val ne: String, val children: js.Array[Tree]) extends js.Object
-//
-//  @ScalaJSDefined
-//  class Node(val id: Int, val label: String, val nodeClass: String) extends js.Object
-//
-//  @ScalaJSDefined
-//  class Edge(val source: Int, val target: Int, val id: String) extends js.Object
-//
-//  @JSName("moveOnZoom")
-//  @js.native
-//  object moveOnZoom extends js.Object {
-//    def apply(svg: js.Any, svgGroup: js.Any): Unit = js.native
-//  }
-//
-//  private def renderText(text: String): Unit = {
-//    println(text)
-//    jQuery.getJSON("parse", js.Dictionary("expression" -> text), { (tree: Tree, textStatus: String, jqXHR: JQueryXHR) =>
-//      jQuery("svg-canvas").empty()
-//
-//      val nodes = new ArrayBuffer[Node]()
-//      val edges = new ArrayBuffer[Edge]()
-//
-//      populate(tree, nodes, edges)
-//
-//      val g = Dagre.newD3Digraph
-//
-//      nodes.foreach(node =>
-//        g.setNode(node.id.toString, js.Dictionary("label" -> node.label, "class" -> node.nodeClass, "rx" -> 5, "ry" -> 5))
-//      )
-//
-//      edges.foreach(edge =>
-//        g.setEdge(edge.source.toString, edge.target.toString, js.Dictionary("lineTension" -> 0.8, "lineInterpolate" -> "bundle"))
-//      )
-//
-//      val render = Dagre.newD3Renderer
-//
-//      val svg = d3.select("#svg-canvas")
-//      val svgGroup = svg.append("g")
-//
-//      render(d3.select("#svg-canvas g"), g)
-//
-//      svgGroup.attr("transform", "translate(5, 5)")
-//      moveOnZoom(svg, svgGroup)
-//    })
-//  }
-//
-//  private def populate(tree: Tree, nodes: ArrayBuffer[Node], edges: ArrayBuffer[Edge]): Node = {
-//    val newNode = new Node(nodes.length, tree.node, "ne-" + tree.ne)
-//
-//    nodes += newNode
-//
-//    tree.children.foreach(child => {
-//      val childNode = populate(child, nodes, edges)
-//      edges += new Edge(newNode.id, childNode.id, newNode.id + "-" + childNode.id)
-//    })
-//
-//    newNode
-//  }
+  @ScalaJSDefined
+  class Tree(val node: String, val ne: String, val children: js.Array[Tree]) extends js.Object
+
+  @ScalaJSDefined
+  class Node(val id: Int, val label: String, val nodeClass: String) extends js.Object
+
+  @ScalaJSDefined
+  class Edge(val source: Int, val target: Int, val id: String) extends js.Object
+
+  @JSName("moveOnZoom")
+  @js.native
+  object moveOnZoom extends js.Object {
+    def apply(svg: js.Any, svgGroup: js.Any): Unit = js.native
+  }
+
+  private def renderText(text: String): Unit = {
+    jQuery.getJSON("parse", js.Dictionary("expression" -> text), { (tree: Tree, textStatus: String, jqXHR: JQueryXHR) =>
+      jQuery("svg-canvas").empty()
+
+      val nodes = new ArrayBuffer[Node]()
+      val edges = new ArrayBuffer[Edge]()
+
+      populate(tree, nodes, edges)
+
+      val g = Dagre.newD3Digraph
+
+      nodes.foreach(node =>
+        g.setNode(node.id.toString, js.Dictionary("label" -> node.label, "class" -> node.nodeClass, "rx" -> 5, "ry" -> 5))
+      )
+
+      edges.foreach(edge =>
+        g.setEdge(edge.source.toString, edge.target.toString, js.Dictionary("lineTension" -> 0.8, "lineInterpolate" -> "bundle"))
+      )
+
+      val render = Dagre.newD3Renderer
+
+      val svg = d3.select("#svg-canvas")
+      val svgGroup = svg.append("g")
+
+      render(d3.select("#svg-canvas g"), g)
+
+      svgGroup.attr("transform", "translate(5, 5)")
+      moveOnZoom(svg, svgGroup)
+    })
+  }
+
+  private def populate(tree: Tree, nodes: ArrayBuffer[Node], edges: ArrayBuffer[Edge]): Node = {
+    val newNode = new Node(nodes.length, tree.node, "ne-" + tree.ne)
+
+    nodes += newNode
+
+    tree.children.foreach(child => {
+      val childNode = populate(child, nodes, edges)
+      edges += new Edge(newNode.id, childNode.id, newNode.id + "-" + childNode.id)
+    })
+
+    newNode
+  }
 }
